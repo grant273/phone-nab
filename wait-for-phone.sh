52fa60908f048116dde50f1a5c60767a3a97b81b
grant273 <grant.klich@gmail.com>
2017-01-08 23:15:05 -0500

    Add files via upload
    uploaded initial version

---
+++ wait-for-phone.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#This script waits for a phone connection and then accesses its data.
+#TODO make script detect phone and not just any USB device
+
+usbcount=$(lsusb | wc -l) 
+newcount=$usbcount
+#wait until phone is connected (not mounted)
+echo 'Waiting for connection...'
+while [ $usbcount -eq $newcount ]; do	
+	newcount=$(lsusb | wc -l)
+done
+
+#mounts phone
+gvfs-mount -li | awk -F= '{if(index($2,"mtp") == 1)system("gvfs-mount "$2)}'
+
+#waits/checks if unlocked
+echo 'Connected. Waiting for unlock...'
+until [ -d /run/user/$UID/gvfs/mtp*/Internal\ storage ]; do	
+	:
+done
+
+#navigates to file system
+cd /run/user/$UID/gvfs/mtp*/Internal\ storage
+ls
+echo 'YOU JUST GOT HACKED M8!!!'
